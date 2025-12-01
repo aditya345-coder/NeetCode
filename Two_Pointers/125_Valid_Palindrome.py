1643319ad62ad16929806e7171603a985b17e099
Aditya Gaharwar <92220550+aditya345-coder@users.noreply.github.com>
2025-12-02 00:57:06 +0530

    Create 125_Valid_Palindrome.py

---
+++ Two_Pointers/125_Valid_Palindrome.py
@@ -0,0 +1,26 @@
+# Link: https://leetcode.com/problems/valid-palindrome/
+
+"""Problem Description:
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, 
+it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+"""
+
+
+# Solution:
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean_org=""
+        for i in range(len(s)):
+            if (s[i].isalnum()):
+                clean_org+=s[i].lower()
+        
+        rev_org=""
+        for i in range(len(clean_org)-1, -1, -1):
+            rev_org+=clean_org[i]
+        
+        return clean_org==rev_org
+
+                
+        
